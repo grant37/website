45df88aad5ab036089a7b7609082d824798858a9
grant37 <grant@lovepopcards.com>
2022-05-16 19:22:05 -0600

    Remove unused context

--- providers/NavProvider.tsx
+++ providers/NavProvider.tsx
@@ -1,17 +1,12 @@
-import { createContext, useContext, useState } from 'react';
+import { useContext } from 'react';
 import Nav from '../components/Nav';
 import { ThemeSettingContext } from './ThemeSettingProvider';
-
-export type NavState = { isDrawerOpen: boolean };
-
-const NavContext = createContext<NavState>({ isDrawerOpen: false });
 
 // Control the Nav state and allow children to read from it
 const NavProvider: React.FC<{
   pageTitle: string;
   children: React.ReactNode;
 }> = ({ pageTitle, children }) => {
-  const [state, setState] = useState<NavState>({ isDrawerOpen: false });
   const [themeSetting, setThemeSetting] = useContext(ThemeSettingContext);
 
   const handleToogleDarkMode = () => {
@@ -19,14 +14,14 @@ const NavProvider: React.FC<{
   };
 
   return (
-    <NavContext.Provider value={state}>
+    <>
       <Nav
         pageTitle={pageTitle}
         isInDarkMode={themeSetting.isDark}
         onToggleDarkMode={handleToogleDarkMode}
       />
       {children}
-    </NavContext.Provider>
+    </>
   );
 };
 
