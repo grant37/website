64c2904b5591629df503fa8327672df9e11a3eeb
grant37 <grant@lovepopcards.com>
2022-05-22 11:28:40 -0600

    Add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,14 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import NavProvider from '../providers/NavProvider';
 import ThemeSettingProvider from '../providers/ThemeSettingProvider';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeSettingProvider>
+      <Head>
+        <link rel='icon' type='image/png' href='/images/favicon.png' />
+      </Head>
       <CssBaseline />
       <NavProvider pageTitle={pageProps.pageTitle} />
       <Container maxWidth='md' sx={{ mb: [2, 4, 6, 8] }}>
